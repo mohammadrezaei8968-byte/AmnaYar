01635005c12bd7731498647fbf540a7751d109c6
mohammadrezaei8968-byte <mohammad.rezaei8968@gmail.com>
2026-09-23 10:15:41 -0700

    Fix gateway web serving and API proxy

--- gateway/server.js
+++ gateway/server.js
@@ -4,17 +4,26 @@ const {createProxyMiddleware}=require("http-proxy-middleware");
 const app=express();
 const PORT=process.env.PORT||10000;
 const API_TARGET=process.env.API_TARGET||"https://amnayar-api.onrender.com";
+const WEB_DIR=path.resolve(__dirname,"../web");
 
 app.use("/api",createProxyMiddleware({
   target:API_TARGET,
   changeOrigin:true,
-  secure:true
+  secure:true,
+  pathRewrite:(p)=>"/api"+p
 }));
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(WEB_DIR));
 
-app.use((req,res)=>{
-  res.sendFile(path.join(__dirname,"public","index.html"));
+app.get("/owner",(req,res,next)=>{
+  res.sendFile(path.join(WEB_DIR,"owner.html"),err=>{ if(err) next(); });
 });
+
+app.use((req,res,next)=>{
+  if(req.path.includes(".")) return next();
+  res.sendFile(path.join(WEB_DIR,"index.html"),err=>{ if(err) next(); });
+});
+
+app.use((req,res)=>res.status(404).send("Not Found"));
 
 app.listen(PORT,"0.0.0.0",()=>console.log("AmnaYar gateway listening on "+PORT));
